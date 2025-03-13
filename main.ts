fbeb223893a50fa0dd1359dcbb5cb54365afbd93
SkeletonGaming518 <thebestgamerever122@gmail.com>
2025-03-12 20:40:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+player.onTravelled(WALK, function () {
+    blocks.place(BLUE_ORCHID, pos(1, 0, -1))
+    blocks.place(POPPY, pos(1, 0, -1))
+    blocks.place(ALLIUM, pos(1, 0, -1))
+    blocks.place(RED_TULIP, pos(1, 0, -1))
+    blocks.place(ORANGE_TULIP, pos(1, 0, -1))
+    blocks.place(WHITE_TULIP, pos(1, 0, -1))
+})
+player.onChat("run", function () {
+	
+})
